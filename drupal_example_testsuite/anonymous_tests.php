4d835632b49d6256d85c5fb918c6bb607fa6236b
ndf <nielsdefeyter@gmail.com>
2016-04-08 16:18:00 +0200

    Added documentation in the Drupal test-suite Examples

--- drupal_example_testsuite/anonymous_tests.php
+++ drupal_example_testsuite/anonymous_tests.php
@@ -1,27 +1,31 @@
 <?php
 
+/**
+ * @file
+ * Example TestSuite with tests for anonymous users.
+ */
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use Crawler\Container;
 use Crawler\Environment;
 use Crawler\Test\Test;
 
+// You can add multiple environments to a test-suite.
 $environments = [];
 $environments[] = new Environment('drupal7', __DIR__ . '/environments/drupal7_local.yml');
 
-
+// For every environment a container is created that runs test-sequences.
+// Each test-sequence runs in a new session.
 foreach ($environments as $env) {
-
-
-  $container = new Container($env, "/tmp/crawler-log.csv", 'Anonymous Tests');
-
-  // Without Garbage.
+  $container = new Container($env, "/tmp/crawler-log.csv", 'Anonymous Tests ' . $env->label());
 
   // Visit frontpage.
   $test_filename = __DIR__ . '/tests/anonymous/anonymous_visit_frontpage.yml';
   $test = new Test($container);
   $test->createTestActionSequenceFromYML($test_filename);
   $container->testSuite()->addTest($test);
+
 
   // Execute the tests.
   $container->testSuite()->doExecute();
--- drupal_example_testsuite/developer_tests.php
+++ drupal_example_testsuite/developer_tests.php
@@ -1,19 +1,24 @@
 <?php
 
+/**
+ * @file
+ * Example TestSuite with tests for admin users.
+ */
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use Crawler\Container;
 use Crawler\Environment;
 use Crawler\Test\Test;
 
+// You can add multiple environments to a test-suite.
 $environments = [];
 $environments[] = new Environment('drupal7', __DIR__ . '/environments/drupal7_local.yml');
 
-
+// For every environment a container is created that runs test-sequences.
+// Each test-sequence runs in a new session.
 foreach ($environments as $env) {
-
-
-  $container = new Container($env, "/tmp/crawler-log.csv", 'Developer Tests');
+  $container = new Container($env, "/tmp/crawler-log.csv", 'Developer Tests ' . $env->label());
 
   /**
    * Without Garbage.
@@ -25,14 +30,14 @@ foreach ($environments as $env) {
   $test->createTestActionSequenceFromYML($test_filename);
   $container->testSuite()->addTest($test);
 
-  // Create and Delete View.
-  $test_filename = __DIR__ . '/tests/developer/developer_views_create_and_delete.yml';
+  // Run Cron.
+  $test_filename = __DIR__ . '/tests/developer/developer_run_cron.yml';
   $test = new Test($container);
   $test->createTestActionSequenceFromYML($test_filename);
   $container->testSuite()->addTest($test);
 
-  // Run Cron.
-  $test_filename = __DIR__ . '/tests/developer/developer_run_cron.yml';
+  // Create and Delete View.
+  $test_filename = __DIR__ . '/tests/developer/developer_views_create_and_delete.yml';
   $test = new Test($container);
   $test->createTestActionSequenceFromYML($test_filename);
   $container->testSuite()->addTest($test);
@@ -41,7 +46,7 @@ foreach ($environments as $env) {
    * With Garbage (cannot remove content)
    */
 
-  // Create Node/Page
+  // Create Node/Page.
   $test_filename = __DIR__ . '/tests/developer/developer_page_create.yml';
   $test = new Test($container);
   $test->createTestActionSequenceFromYML($test_filename);
@@ -50,6 +55,4 @@ foreach ($environments as $env) {
 
   // Execute the tests.
   $container->testSuite()->doExecute();
-
-
 }
